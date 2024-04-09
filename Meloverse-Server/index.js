9e936b40e49f78b65f3c16c2b609be8b9ffa4afe
1250523689@qq.com <1250523689@qq.com>
2024-04-09 11:18:35 -0500

    Co-authored-by: Arren <Arren12@users.noreply.github.com> Co-authored-by: Jamaldeeen Abdulazeez <Cyrol96@users.noreply.github.com> Co-authored-by: Jay <jaysoleson@users.noreply.github.com>

---
+++ Meloverse-Server/index.js
@@ -0,0 +1,38 @@
+//--------------------Configure Express, routes----------------------
+const express = require("express");
+const path=require('path')
+
+const app=express()
+app.use(express.static(__dirname+'/public'));
+app.use('views',express.static(path.join(__dirname,'views')));
+app.set('view engine','hjs');
+const PORT=process.env.PORT || 5000;
+
+const indexRoutes=require('./routes/index')
+app.use('/',indexRoutes);
+
+
+
+//--------------------Configure MongoDB And Mongoose----------------------
+const mongoose = require("mongoose");
+
+//implement mongoDB atlas connection string
+const DriverString = "mongodb://localhost:27017/meloverse";
+mongoose
+  .connect(DriverString)
+  .then((data) => console.log("Yay, MongoDB connection success"))
+  .catch((err) => console.log("Oh no, MongoDB connection error:", err.message));
+
+
+
+
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404: Not Found</h1>");
+});
+
+const server=app.listen(PORT,()=>{
+  console.log(`server running on port ${server.address().port}`);
+  console.log(`http://localhost:${server.address().port}`);
+})
+
